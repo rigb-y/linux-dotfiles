380de84cd60c823de6e739bcf7607d9b45b6c0dc
rigb-y <noodlecloudcode@gmail.com>
2026-08-24 17:00:04 -0500

    nvim gruv rework

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,12 +6,27 @@ vim.cmd([[set autoindent]])
 -- vim.cmd("colorscheme retrobox")
 -- vim.cmd("colorscheme unokai")
 
-vim.cmd("colorscheme monokai-pro-classic")
+-- Note: gruvYellow guifg=#fabd2f
+vim.cmd("colorscheme gruv")
 vim.cmd[[
-    hi normal guibg=#171717
+    hi gruvGreenBold guifg=#579666
+    hi gruvYellow guifg=#ffaac3
+    hi gruvOrange guifg=#ffaac3
     hi LineNr guifg=#bdbdbd guibg=#171717
     hi SignColumn guibg=#171717
+    hi delimiter guifg=#964246
+    hi Operator guifg=#69B6B8
+
+    hi gruvFg1 guifg=ebdbb2
+    hi def link @lsp.type.parameter.c gruvFg1
 ]]
+
+-- vim.cmd("colorscheme monokai-pro-classic")
+-- vim.cmd[[
+--     hi normal guibg=#171717
+--     hi LineNr guifg=#bdbdbd guibg=#171717
+--     hi SignColumn guibg=#171717
+-- ]]
 
 -- vim.cmd("colorscheme delek")
 -- vim.cmd[[
@@ -45,7 +60,6 @@ vim.cmd[[
     let @b = "^i\\item\<Space>bold\<M-Tab>\<Esc>sa\<BS>\<Esc>f:\<Ignore>i}\<Esc>h"
     let @c = "^i\\item\<Space>\<Esc>h"
     let @d = "^f}\<Ignore>f{\<Ignore>i\<CR>\<Esc>"
-
 ]]
 
 -- vim.api.nvim_create_user_command("Colorschemes", function()
@@ -56,4 +70,3 @@ vim.cmd[[
 --     end
 --   end)
 -- end, {})
---
